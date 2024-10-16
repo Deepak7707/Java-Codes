33aa91fa36761c0a408bd8e335a58c8fd05c5ca6
Deepak Sharma <88672612+Deepak7707@users.noreply.github.com>
2024-10-16 21:49:11 +0530

    Add files via upload

---
+++ forloops1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class forloops1{
+
+    public static void main(String[] args) {
+           Scanner s= new Scanner(System.in);
+        int n= s.nextInt();
+        
+        for(int i=0; i<=n; i+=7){
+            
+               
+                System.out.print(i+" ");
+            }
+    
+              
+        }
+        
+    }
